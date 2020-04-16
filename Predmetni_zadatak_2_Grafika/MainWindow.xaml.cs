d0aaccada474ec9b0a75fd23800a26136a7ac255
Srdjan Stankov <stankovsr@gmail.com>
2020-04-16 20:41:06 +0200

    Substations, nodes and switches are now drawn when winow loads

--- Predmetni_zadatak_2_Grafika/MainWindow.xaml.cs
+++ Predmetni_zadatak_2_Grafika/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -16,23 +18,12 @@ namespace Predmetni_zadatak_2_Grafika
     public partial class MainWindow : Window
     {
         private const int ROW_HEIGHT_COUNT = 100;
+        private HashSet<(uint, uint)> usedCoords = new HashSet<(uint, uint)>();
 
         public MainWindow()
         {
             InitializeComponent();
-
-            for (int i = 0; i < ROW_HEIGHT_COUNT; i++)
-            {
-                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(window.Width / ROW_HEIGHT_COUNT) });
-                grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(window.Height / ROW_HEIGHT_COUNT) });
-            }
-
-            var element = new Rectangle() { Width = 50, Height = 50, Fill = Brushes.Red };
-            grid.Children.Add(element);
-            Grid.SetRow(element, 5);
-            Grid.SetColumn(element, 9);
-
-            LoadXml();
+            window.WindowState = WindowState.Maximized;
         }
 
         private void LoadXml()
@@ -41,23 +32,90 @@ namespace Predmetni_zadatak_2_Grafika
             doc.Load("Geographic.xml");
 
             var nodeList = doc.DocumentElement.SelectNodes("/NetworkModel/Substations/SubstationEntity");
+            var substations = new List<SubstationEntity>();
+            CreateElementsOnGui<SubstationEntity, Rectangle>(nodeList, substations, Brushes.Red);
 
-            foreach (XmlNode item in nodeList)
-            {
-                var x = double.Parse(item.SelectSingleNode("X").InnerText);
-                var y = double.Parse(item.SelectSingleNode("Y").InnerText);
+            nodeList = doc.DocumentElement.SelectNodes("/NetworkModel/Nodes/NodeEntity");
+            var nodes = new List<NodeEntity>();
+            CreateElementsOnGui<NodeEntity, Ellipse>(nodeList, nodes, Brushes.Blue);
 
-                ConvertToNearestGrid(x, y);
-            }
-            
-            
-            
-            MessageBox.Show(nodeList.Count.ToString());
+            nodeList = doc.DocumentElement.SelectNodes("/NetworkModel/Switches/SwitchEntity");
+            var switches = new List<SwitchEntity>();
+            CreateElementsOnGui<SwitchEntity, Rectangle>(nodeList, switches, Brushes.Green);
         }
 
-        private void ConvertToNearestGrid(double x, double y)
+        private void CreateElementsOnGui<T1, T2>(XmlNodeList nodeList, List<T1> entityList, Brush fill) where T1 : PowerEntity, new() where T2 : Shape, new()
         {
+            foreach (XmlNode item in nodeList)
+            {
+                double xd = double.Parse(item.SelectSingleNode("X").InnerText, CultureInfo.InvariantCulture);
+                double yd = double.Parse(item.SelectSingleNode("Y").InnerText, CultureInfo.InvariantCulture);
+
+                uint x = uint.Parse(Math.Round(xd %= ROW_HEIGHT_COUNT).ToString());
+                uint y = uint.Parse(Math.Round(yd %= ROW_HEIGHT_COUNT).ToString());
+
+                (x, y) = FindClosestXY(x, y);
+
+                var el = new T2() { Fill = fill };
+
+                Grid.SetRow(el, Convert.ToInt32(x));
+                Grid.SetColumn(el, Convert.ToInt32(y));
+                grid.Children.Add(el);
+
+
+                entityList.Add(new T1() { X = x, Y = y });
+            }
+        }
+
+        private (uint x, uint y) FindClosestXY(uint x, uint y)
+        {
+            if (!usedCoords.Contains((x, y)))
+            {
+                usedCoords.Add((x, y));
+                return (x, y);
+            }
+
+            uint newX = --x;
+            newX = (newX == uint.MaxValue) ? ROW_HEIGHT_COUNT : newX;
+            uint newY = --y;
+            newY = (newY == uint.MaxValue) ? ROW_HEIGHT_COUNT : newY;
             
+            while (usedCoords.Contains((newX, newY)))
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (!usedCoords.Contains((newX, newY)))
+                        {
+                            goto WhileExit;
+                        }
+                        newY++;
+                    }
+                    if (!usedCoords.Contains((newX, newY)))
+                    {
+                        goto WhileExit;
+                    }
+                    newX++;
+                    newY -= 2;
+                }
+
+            }
+
+            WhileExit:
+            usedCoords.Add((newX, newY));
+            return (newX, newY);
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < ROW_HEIGHT_COUNT; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(window.Width / ROW_HEIGHT_COUNT) });
+                grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(window.Height / ROW_HEIGHT_COUNT) });
+            }
+
+            LoadXml();
         }
     }
 }
